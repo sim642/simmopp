7b2f48ad42c0315e89a6b7cb9bb53bad2337fb05
Simmo Saan <simmo.saan@gmail.com>
2016-03-03 14:11:40 +0200

    Add utility for type displaying

---
+++ include/simmo/utility.hpp
@@ -0,0 +1,16 @@
+//
+// Created by simmo on 3.03.16.
+//
+
+#ifndef SIMMOPP_UTILITY_HPP
+#define SIMMOPP_UTILITY_HPP
+
+namespace simmo
+{
+
+template<typename T>
+struct type_displayer;
+
+}
+
+#endif //SIMMOPP_UTILITY_HPP
