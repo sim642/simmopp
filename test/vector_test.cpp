c1079cd4b407367d2003a280b2b8b0613fd57a4c
Simmo Saan <simmo.saan@gmail.com>
2016-06-08 23:41:22 +0300

    Test vector norm/dot/cross

--- test/vector_test.cpp
+++ test/vector_test.cpp
@@ -110,3 +110,41 @@ TEST(vector_test, assign_mult_div)
     vector2i half(4, -30);
     ASSERT_EQ(v /= 2, half);
 }
+
+TEST(vector_test, norm)
+{
+    vector1i v1(7);
+    EXPECT_FLOAT_EQ(norm(v1), 7);
+
+    vector2i v2(1, 2);
+    EXPECT_FLOAT_EQ(norm(v2), sqrt(1 * 1 + 2 * 2));
+
+    vector3i v3(5, 6, -8);
+    EXPECT_FLOAT_EQ(norm(v3), sqrt(5 * 5 + 6 * 6 + 8 * 8));
+}
+
+TEST(vector_test, dot)
+{
+    int d;
+
+    vector1i a1(2), b1(-5);
+    d = dot(a1, b1);
+    EXPECT_EQ(d, 2 * -5);
+
+    vector2i a2(1, 2), b2(5, 6);
+    d = dot(a2, b2);
+    EXPECT_EQ(d, 1 * 5 + 2 * 6);
+
+    vector3i a3(1, 2, 3), b3(-1, 5, -10);
+    d = dot(a3, b3);
+    EXPECT_EQ(d, 1 * -1 + 2 * 5 + 3 * -10);
+}
+
+TEST(vector_test, cross)
+{
+    vector3i a(1, 2, 3), b(-1, 5, -10);
+
+    vector3i actual = cross(a, b);
+    vector3i expected(-35, 7, 7);
+    EXPECT_EQ(actual, expected);
+}
