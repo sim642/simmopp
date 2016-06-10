7717334c06f60960e9cb509cd63fd87cb80a73c6
Simmo Saan <simmo.saan@gmail.com>
2016-06-10 17:49:10 +0300

    Test basic_vector equals/hash/comparison

--- test/point_test.cpp
+++ test/point_test.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
 #include <simmo/point.hpp>
+#include <simmo/basic_vector/comparison.hpp>
 
 using namespace simmo;
 
@@ -38,4 +39,42 @@ TEST(point_test, vector_assign_plus_minus)
 
     point2i diff(1, 3);
     ASSERT_EQ(A -= b, diff);
+}
+
+TEST(point_test, equals)
+{
+    point2i A(1, -2), B(1, -2), C(1, 2);
+
+    EXPECT_EQ(A, B);
+    EXPECT_EQ(B, A);
+
+    /*EXPECT_NE(A, C);
+    EXPECT_NE(B, C);*/
+}
+
+TEST(point_test, hash)
+{
+    point2i A(1, -2), B(1, -2);
+
+    auto hashA = std::hash<point2i>()(A);
+    auto hashB = std::hash<point2i>()(B);
+
+    EXPECT_EQ(hashA, hashB);
+}
+
+TEST(point_test, comparison)
+{
+    point2i A(1, 2), B(1, 3), C(2, 3);
+
+    EXPECT_TRUE(A < B);
+    EXPECT_TRUE(A < C);
+    EXPECT_TRUE(B < C);
+
+    EXPECT_FALSE(B < A);
+    EXPECT_FALSE(C < A);
+    EXPECT_FALSE(C < B);
+
+    EXPECT_FALSE(A < A);
+    EXPECT_FALSE(B < B);
+    EXPECT_FALSE(C < C);
 }
