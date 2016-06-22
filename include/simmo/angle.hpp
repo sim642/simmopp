0508a965e7ff4b2ea515c954ddfc303d47000576
Simmo Saan <simmo.saan@gmail.com>
2016-06-22 17:48:32 +0300

    Add angle arithmetic

--- include/simmo/angle.hpp
+++ include/simmo/angle.hpp
@@ -38,9 +38,73 @@ public:
         return rad_value / M_PI * 180;
     }
 
+    auto operator +() const
+    {
+        return *this;
+    }
+
+    auto operator -() const
+    {
+        return angle(-rad_value);
+    }
+
+    auto& operator +=(const angle &other)
+    {
+        rad_value += other.rad_value;
+        return *this;
+    }
+
+    auto& operator -=(const angle &other)
+    {
+        rad_value -= other.rad_value;
+        return *this;
+    }
+
+    auto& operator *=(value_type factor)
+    {
+        rad_value *= factor;
+        return *this;
+    }
+
+    auto& operator /=(value_type factor)
+    {
+        rad_value /= factor;
+        return *this;
+    }
+
 private:
     value_type rad_value;
 };
+
+angle operator +(const angle &lhs, const angle &rhs)
+{
+    angle ret = lhs;
+    return ret += rhs;
+}
+
+angle operator -(const angle &lhs, const angle &rhs)
+{
+    angle ret = lhs;
+    return ret -= rhs;
+}
+
+angle operator *(const angle &lhs, angle::value_type rhs)
+{
+    angle ret = lhs;
+    return ret *= rhs;
+}
+
+angle operator *(angle::value_type lhs, const angle &rhs)
+{
+    angle ret = rhs;
+    return ret *= lhs;
+}
+
+angle operator /(const angle &lhs, angle::value_type rhs)
+{
+    angle ret = lhs;
+    return ret /= rhs;
+}
 
 }
 
--- test/angle_test.cpp
+++ test/angle_test.cpp
@@ -13,7 +13,6 @@ TEST(angle_test, constructor)
     EXPECT_FLOAT_EQ(b.deg(), 12.5);
 }
 
-
 TEST(angle_test, conversion)
 {
     angle a = angle::deg(45);
@@ -21,4 +20,53 @@ TEST(angle_test, conversion)
 
     angle b = angle::rad(1);
     EXPECT_FLOAT_EQ(b.deg(), 57.29578);
+}
+
+TEST(angle_test, plus_minus)
+{
+    angle a = angle::deg(90);
+    angle b = angle::deg(30);
+
+    EXPECT_FLOAT_EQ((a + b).deg(), 120);
+    EXPECT_FLOAT_EQ((b + a).deg(), 120);
+
+    EXPECT_FLOAT_EQ((a - b).deg(), 60);
+    EXPECT_FLOAT_EQ((b - a).deg(), -60);
+}
+
+TEST(angle_test, assign_plus_minus)
+{
+    angle a = angle::deg(90);
+    EXPECT_FLOAT_EQ(a.deg(), 90);
+
+    a += angle::deg(10);
+    EXPECT_FLOAT_EQ(a.deg(), 100);
+
+    a -= angle::deg(55);
+    EXPECT_FLOAT_EQ(a.deg(), 45);
+}
+
+TEST(angle_test, multiply_divide)
+{
+    angle a = angle::deg(90);
+
+    EXPECT_FLOAT_EQ((2 * a).deg(), 180);
+    EXPECT_FLOAT_EQ((a * 2).deg(), 180);
+    EXPECT_FLOAT_EQ((a / 0.5).deg(), 180);
+
+    EXPECT_FLOAT_EQ((0.5 * a).deg(), 45);
+    EXPECT_FLOAT_EQ((a * 0.5).deg(), 45);
+    EXPECT_FLOAT_EQ((a / 2).deg(), 45);
+}
+
+TEST(angle_test, assign_multiply_divide)
+{
+    angle a = angle::deg(90);
+    EXPECT_FLOAT_EQ(a.deg(), 90);
+
+    a *= 2;
+    EXPECT_FLOAT_EQ(a.deg(), 180);
+
+    a /= 4;
+    EXPECT_FLOAT_EQ(a.deg(), 45);
 }
